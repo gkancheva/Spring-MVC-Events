a4ff5334a38d2403766d9fc3cb59d062fc31fdaa
g_kancheva <g_kancheva@live.com>
2016-08-25 00:47:35 +0300

    Adding security and registration functionality.

---
+++ src/main/java/eventSystem/services/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package eventSystem.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/img/**").permitAll()
+                .antMatchers("/js/**").permitAll()
+                .antMatchers("/", "/events", "/users/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/users/login").permitAll()
+                .loginProcessingUrl("/")
+                .failureUrl("/users/login")
+                .and()
+            .logout().permitAll()
+                .logoutSuccessUrl("/");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("USER");
+    }
+}
